00b629bba97d3937f16a0581640d1c67b4252834
zhuxy <zhuxy@channelsoft>
2018-12-14 17:50:06 +0800

    1

--- servicecloud-consumer-dept-80/src/main/java/com/atguigu/controller/DeptController_Consumer.java
+++ servicecloud-consumer-dept-80/src/main/java/com/atguigu/controller/DeptController_Consumer.java
@@ -1,5 +1,43 @@
 package com.atguigu.controller;
 
-public class DeptController_Consumer {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.atguigu.springcloud.entities.Dept;
+
+@RestController
+public class DeptController_Consumer {
+	
+	private static final String REST_PREF_URL = "http://MICROSERVICECLOUD-DEPT";
+    /**
+     * restTemplate 模板发送请求，http协议
+     */
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	@RequestMapping("/consumer/dept/add")
+	public boolean add(Dept dept){
+		return restTemplate.postForObject(REST_PREF_URL+"/dept/add", dept, Boolean.class);
+	}
+	
+	@RequestMapping("/consumer/dept/get/{id}")
+	public Dept get(@PathVariable("id")Long id){
+		return restTemplate.getForObject(REST_PREF_URL+"/dept/get/"+ id, Dept.class);
+	}
+	
+	@RequestMapping("consumer/dept/list")
+	public List<Dept> list(){
+		return restTemplate.getForObject(REST_PREF_URL+"/dept/list", List.class);
+	}
+	// 测试@EnableDiscoveryClient,消费端可以调用服务发现
+	@RequestMapping("/consumer/dept/discovery")
+	public Object discovery(){
+		return restTemplate.getForObject(REST_PREF_URL+"/dept/discovery", Object.class);
+	}
 }
--- servicecloud-provider-dept-8001/src/main/java/com/atguigu/springcloud/DeptProvider8001_App.java
+++ servicecloud-provider-dept-8001/src/main/java/com/atguigu/springcloud/DeptProvider8001_App.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-//@EnableEurekaClient  //启动后将此服务注册进eureka注册中心
+@EnableEurekaClient  //启动后将此服务注册进eureka注册中心
 //@EnableDiscoveryClient //服务发现
 public class DeptProvider8001_App {
 	//主启动类
