2550200cf9302f0f90b72ed1f121fdd8bb71155a
zhuxy <zhuxy@channelsoft>
2018-12-25 18:10:35 +0800

    提供服务者集群

---
+++ servicecloud-provider-dept-8002/src/main/java/com/atguigu/springcloud/DeptProvider8002_App.java
@@ -0,0 +1,16 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient  //启动后将此服务注册进eureka注册中心
+//@EnableDiscoveryClient //服务发现
+public class DeptProvider8002_App {
+	//主启动类
+    public static void main(String[] args) {
+		SpringApplication.run(DeptProvider8002_App.class, args);
+	}
+}
